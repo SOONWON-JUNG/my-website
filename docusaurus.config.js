c3244125475e1a7bb371edf7fb0a06f5dbdc8ee5
SOONWONJUNG <wk7178wk@gmail.com>
2024-10-03 18:52:25 +0900

    디자인 변경 테스트

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'ALPS SSG PROTOTYPE',
+  tagline: 'Docusaurus are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -76,7 +76,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'ALPS SSG PROTOTYPE',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -86,7 +86,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'ALPS FAQ',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
